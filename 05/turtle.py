570302eadc0854f776ac9d7c135b0bb16cbb15ae
emldemld <85053469+emldemld@users.noreply.github.com>
2021-09-14 19:08:14 +0900

    turtle.py
    https://youtu.be/bfmpXYaDq68

---
+++ 05/turtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+def d_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+def w_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+def a_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+def s_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+def restart():
+    turtle.reset()
+    
+turtle.shape('turtle')
+turtle.onkey(d_move, 'd')
+turtle.onkey(w_move, 'w')
+turtle.onkey(s_move, 's')
+turtle.onkey(a_move, 'a')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
