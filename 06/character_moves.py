f55743a240310467e84fe90cf99b8174f1b13254
emldemld <85053469+emldemld@users.noreply.github.com>
2021-09-16 19:37:46 +0900

    character_moves

---
+++ 06/character_moves.py
@@ -0,0 +1,59 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def drawcanvas(x, y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+
+x = 400
+y = 90
+angle = -90
+
+while True:
+    while x < 780:
+        drawcanvas(x, y)
+        x += 10
+        delay(0.01)
+    while y < 560:
+        drawcanvas(x, y)
+        y += 10
+        delay(0.01)
+    while x > 20:
+        drawcanvas(x, y)
+        x -= 10
+        delay(0.01)
+    while y > 90:
+        drawcanvas(x, y)
+        y -= 10
+        delay(0.01)
+    while x < 400:
+        drawcanvas(x, y)
+        x += 10
+        delay(0.01)    
+    x = 400
+    y = 290
+    while angle < 270:
+        cos = math.cos(angle / 360 * 2 * math.pi) * 200
+        sin = math.sin(angle / 360 * 2 * math.pi) * 200
+        drawcanvas(cos + x, sin + y)
+        angle+=10
+        delay(0.02)
+    angle = -90
+    x = 400
+    y = 90
+
+    
+close_canvas()
